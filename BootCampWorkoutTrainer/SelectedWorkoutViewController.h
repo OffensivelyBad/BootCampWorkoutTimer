7eb90667b44de91851f5f1d46bc8823fb442c77a
SRoller <sroller@justfab.com>
2014-04-24 00:24:36 -0400

    added new view

---
+++ BootCampWorkoutTrainer/SelectedWorkoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectedWorkoutViewController.h
+//  BootCampWorkoutTrainer
+//
+//  Created by Shawn Roller on 4/22/14.
+//  Copyright (c) 2014 Shawn Roller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectedWorkoutViewController : UIViewController
+
+@end
